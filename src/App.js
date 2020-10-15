9ec033df60fcfff7805e657a7167f85871707381
SofyanNurRohman <sofyannurrohman1@gmail.com>
2020-10-15 19:45:46 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import "assets/scss/style.scss";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LandingPages from 'pages/LandingPages';
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route path="/" component={LandingPages}></Route>
+      </Router>
     </div>
   );
 }
