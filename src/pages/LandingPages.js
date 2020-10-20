c2553535450fdf599aa91e0fe2fcbe7c4efd0830
SofyanNurRohman <sofyannurrohman1@gmail.com>
2020-10-20 19:43:57 +0700

    Testimony

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -4,21 +4,24 @@ import landingPage from 'json/landingPage.json'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import Categories from 'parts/Categories'
+import Testimony from 'parts/Testimony'
 export default class LandingPages extends Component {
     constructor(props){
         super(props);
         this.refMostPicked= React.createRef();
     }
     render() {
-    return ( 
-        <>
-         <Header {...this.props}></Header>   
-         <Hero  refMostPicked={this.refMostPicked}data={landingPage.hero} />
-         <MostPicked
-         refMostPicked={this.refMostPicked} 
-         data={landingPage.mostPicked}/>
-         <Categories data={landingPage.categories} />
-        </>
-        )
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+      </>
+    );
 }
 }
